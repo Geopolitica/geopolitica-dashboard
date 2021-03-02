58359b62bca1b46eedb7ab5a3877b54321a187ad
brittwitham <brittany.witham@gmail.com>
2021-03-02 11:32:04 -0500

    Final changes before deployment

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const eventsRouter = require("./backend/routes/eventRouter");
-const mapRouter = require("./backend/routes/mapRouter");
+const eventsRouter = require("./routes/eventRouter");
+const mapRouter = require("./routes/mapRouter");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -25,9 +26,15 @@ mongoose
 app.use("/events", eventsRouter);
 app.use("/map", mapRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
